a3f919840a9b44786e5e3feabb31fe13cc2d53ae
electrone901 <carbajalluis@rocketmail.com>
2020-09-28 01:11:33 -0400

    adding: process.env to heroku

--- server/auth/google.js
+++ server/auth/google.js
@@ -3,7 +3,6 @@ const router = require('express').Router()
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const {User} = require('../db/models')
 module.exports = router
-const {clientID, clientSecret, callback} = require('../../secrets') // imports api keys
 
 /**
  * For OAuth keys and other secrets, your Node process will search
@@ -18,49 +17,58 @@ const {clientID, clientSecret, callback} = require('../../secrets') // imports a
  * process.env.GOOGLE_CLIENT_SECRET = 'your google client secret'
  * process.env.GOOGLE_CALLBACK = '/your/google/callback'
  */
-let googleConfig = {}
 
-if (process.env.GOOGLE_CLIENT_ID || process.env.GOOGLE_CLIENT_SECRET) {
-  googleConfig = {
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  console.log('Google client ID / secret not found. Skipping Google OAuth.')
+} else {
+  const googleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://beats-store.herokuapp.com/'
+    callbackURL: process.env.GOOGLE_CALLBACK
   }
-} else {
-  googleConfig = {
-    clientID: clientID,
-    clientSecret: clientSecret,
-    callbackURL: callback
-  }
+
+  const strategy = new GoogleStrategy(
+    googleConfig,
+    (token, refreshToken, profile, done) => {
+      const googleId = profile.id
+      const email = profile.emails[0].value
+      const imgUrl = profile.photos[0].value
+      const firstName = profile.name.givenName
+      const lastName = profile.name.familyName
+      const fullName = profile.displayName
+
+      User.findOrCreate({
+        where: {googleId},
+        defaults: {email, imgUrl, firstName, lastName, fullName}
+      })
+        .then(([user]) => done(null, user))
+        .catch(done)
+    }
+  )
+
+  passport.use(strategy)
+
+  router.get(
+    '/',
+    passport.authenticate('google', {scope: ['email', 'profile']})
+  )
+
+  //  GOOGLE MIDDLEWARE ONE WAY
+  // router.get(
+  //   '/callback',
+  //   passport.authenticate('google', {
+  //     successRedirect: '/home',
+  //     failureRedirect: '/login'
+  //   })
+  // )
+
+  //  GOOGLE MIDDLEWARE REFACTORED
+  router.get(
+    '/callback',
+    passport.authenticate('google', {failureRedirect: '/login'}),
+    function(req, res) {
+      console.log('test')
+      res.redirect('/home')
+    }
+  )
 }
-
-const strategy = new GoogleStrategy(
-  googleConfig,
-  (token, refreshToken, profile, done) => {
-    const googleId = profile.id
-    const email = profile.emails[0].value
-    const imgUrl = profile.photos[0].value
-    const firstName = profile.name.givenName
-    const lastName = profile.name.familyName
-    const fullName = profile.displayName
-
-    User.findOrCreate({
-      where: {googleId},
-      defaults: {email, imgUrl, firstName, lastName, fullName}
-    })
-      .then(([user]) => done(null, user))
-      .catch(done)
-  }
-)
-
-passport.use(strategy)
-
-router.get('/', passport.authenticate('google', {scope: ['email', 'profile']}))
-
-router.get(
-  '/callback',
-  passport.authenticate('google', {
-    successRedirect: '/home',
-    failureRedirect: '/login'
-  })
-)
